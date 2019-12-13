00f560d17e5ab7710e5525ccda086a441bb0e20b
alvarowolfx <alvarowolfx@gmail.com>
2019-12-13 00:36:18 -0400

    feat: Increase interval to 10 min

--- firmware/config.py
+++ firmware/config.py
@@ -3,5 +3,5 @@ ECHO_PIN = 0  # D3 pin / GPIO0
 LED_PIN = 2  # D4 pin / GPIO2
 WIFI_SSID = 'your-wifi-network-name'
 WIFI_PASSWORD = 'your-wifi-password'
-INTERVAL = 5 * 60
+INTERVAL = 10 * 60
 URL = 'your-backend-url'
